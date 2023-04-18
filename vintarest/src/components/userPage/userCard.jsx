29713c24205880cf1cb5d413dcd907c382275c20
Camilo Osorio <camilo.osorio.ca@gmail.com>
2023-04-18 01:21:39 -0500

    update

--- vintarest/src/components/userPage/userCard.jsx
+++ vintarest/src/components/userPage/userCard.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import userIcon from "../../assets/person-circle.svg";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase/config";
 
 export const UserCard = () => {
-	console.log(auth.currentUser);
+	const [user, setUSer] = useState("");
+	const [name, setName] = useState("");
+
+	useEffect(() => {
+		onAuthStateChanged(auth, () => {
+			setName(auth.currentUser.displayName.split("/")[0]);
+			setUSer(auth.currentUser.displayName.split("/")[1]);
+		});
+	}, []);
 	return (
 		<>
 			<div
@@ -13,8 +23,8 @@ export const UserCard = () => {
             p-3 flex flex-col place-items-center"
 			>
 				<img src={userIcon} alt="" className="h-[120px] mb-2 select-none" />
-				<h1 className="font-semibold text-[32px] text-center">{auth.currentUser.displayName.split('/')[0]}</h1>
-				<h2 className="text-[14px]">{`@${auth.currentUser.displayName.split('/')[1]}`}</h2>
+				<h1 className="font-semibold text-[32px] text-center">{name}</h1>
+				<h2 className="text-[14px]">{`@${user}`}</h2>
 			</div>
 		</>
 	);
