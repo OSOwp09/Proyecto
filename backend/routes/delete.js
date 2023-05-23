3fba540ec7034c331fa3fa6ef81f8071faf5d03e
Camilo Osorio <camilo.osorio.ca@gmail.com>
2023-05-22 20:36:39 -0500

    remove

--- backend/routes/delete.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {deleteUser} = require("../Controllers/delete")
-
-router.post("/deleteUser", deleteUser)
-
-module.exports = router;
